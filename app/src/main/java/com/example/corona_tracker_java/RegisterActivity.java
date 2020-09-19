09e12f375f903198498faa2431192a14155d2dba
TipsyKinkajou52 <sorockminecraft@outlook.com>
2020-09-19 17:40:08 -0400

    firebase database error issue

--- app/src/main/java/com/example/corona_tracker_java/RegisterActivity.java
+++ app/src/main/java/com/example/corona_tracker_java/RegisterActivity.java
@@ -1,23 +1,36 @@
 package com.example.corona_tracker_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
     EditText mTextUsername;
     EditText mTextPassword;
     EditText getTextConfPassword;
     Button mButtonRegister;
     Button mButtontaketologinpage;
     TextView mTextViewLogin;
+
+    private FirebaseAuth mAuth;
+    private EditText inputEmail, inputPassword
+    private Button btnSignUp, btnResetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +41,42 @@ public class RegisterActivity extends AppCompatActivity {
         mTextPassword = (EditText) findViewById(R.id.edittext_password);
         mTextPassword = (EditText) findViewById(R.id.edittext_conf_password);
         mButtonRegister = (Button) findViewById(R.id.button_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        inputEmail = (EditText) findViewById(R.id.edittext_email);
+        inputPassword = (EditText) findViewById(R.id.edittext_password);
+        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
         addListenerOnButton();
     }
 
+    public void updateUI(FirebaseUser currentUser) {
+        if(currentUser != null){
+            Toast.makeText(this,"U Signed In successfully",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this,GoogleMapsTracker.class));
+        }else {
+            Toast.makeText(this,"U Didnt signed in",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    btnSignUp.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String email = inputEmail.getText().toString().trim();
+            String password = inputPassword.getText().toString().trim();
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (password.length() < 6) {
+                Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
 
     public void addListenerOnButton() {
 
@@ -50,4 +96,6 @@ public class RegisterActivity extends AppCompatActivity {
 
         });
     }
+
+
 }
