3a0de1affc828b7c0bf1081527872af9096c23ef
TipsyKinkajou52 <sorockminecraft@outlook.com>
2020-09-19 16:48:50 -0400

    firebase database updateUI addition

--- app/src/main/java/com/example/corona_tracker_java/MainActivity.java
+++ app/src/main/java/com/example/corona_tracker_java/MainActivity.java
@@ -45,10 +45,8 @@ public class MainActivity extends AppCompatActivity {
         addListenerOnButtonLogin();
         addListenerOnButtonGMapAPIDevs();
     }
+
     @Override
-
-
-
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -56,18 +54,12 @@ public class MainActivity extends AppCompatActivity {
         updateUI(currentUser);
     }
 
-    public void onComplete(@NonNull Task<AuthResult> task) {
-        if (task.isSuccessful()) {
-            // Sign in success, update UI with the signed-in user's information
-            Log.d(TAG, "signInWithEmail:success");
-            FirebaseUser user = mAuth.getCurrentUser();
-            updateUI(user);
-        } else {
-            // If sign in fails, display a message to the user.
-            Log.w(TAG, "signInWithEmail:failure", task.getException());
-            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                    Toast.LENGTH_SHORT).show();
-            updateUI(null);
+    public void updateUI(FirebaseUser currentUser) {
+        if(currentUser != null){
+            Toast.makeText(this,"U Signed In successfully",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this,GoogleMapsTracker.class));
+        }else {
+            Toast.makeText(this,"U Didnt signed in",Toast.LENGTH_LONG).show();
         }
     }
 
